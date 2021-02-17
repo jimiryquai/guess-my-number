324133b88eaadefafe0904484cd88f947efc1b5e
James Ryan <james.ryan@pathwayforcare.com>
2021-02-17 20:01:12 +0000

    Added scoring logic and min max values on html input

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ const getRandomIntInclusive = function (min, max) {
 
 const secretNumber = getRandomIntInclusive(1, 20);
 document.querySelector('.number').textContent = secretNumber;
-
-document.querySelector('.score').textContent = 20;
-// document.querySelector('.guess').value = 23;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -18,10 +16,25 @@ document.querySelector('.check').addEventListener('click', function () {
     if (!guess) {
         document.querySelector('.message').textContent = '⛔ No Number';
     } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent ='🎉 Correct Number!';
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('.highscore').textContent = score;
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = 'Too High!';
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too High!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'Game Over!';
+            document.querySelector('.score').textContent = 0;
+        }
     } else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = 'Too Low!';
+        if (score > 0) {
+            document.querySelector('.message').textContent = 'Too Low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'Game Over!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 })
