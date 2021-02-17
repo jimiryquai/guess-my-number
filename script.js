88c9584bcbe2cd4454a7917f2349bafa78f442f9
James Ryan <james.ryan@pathwayforcare.com>
2021-02-17 20:16:12 +0000

    Added page reload functionality to again button

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ const secretNumber = getRandomIntInclusive(1, 20);
 document.querySelector('.number').textContent = secretNumber;
 let score = 20;
 
+document.querySelector('.again').addEventListener('click', function () {
+location.reload();
+return false;
+});
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
 
